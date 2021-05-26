13980557344276abbb0a36dc9d2b0722b48c16ff
surya <sbistaiw@gmail.com>
2021-05-26 23:10:03 +0545

    Added the date validator

--- WebApp/models.py
+++ WebApp/models.py
@@ -1,17 +1,19 @@
-from django.core.checks import messages
-from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from datetime import datetime, date
-from django.core.exceptions import ValidationError
-from django.http import HttpResponse
 import datetime
+from time import timezone
 
-blood_group=[("1","A+"),("2","A-"),("3","B+"),("4","B-"),("5","AB+"),("6","AB-"),("3","O+"),("4","O-")]
-genderList=[("1","Male"),("2","Female"),("3","Others")]
-caseList=[("1","Accident"),("2","Delivery"),("3","Anemia"),("4","Dialysis"),("5","Operation"),("6","Urgent"),("7","Others")]
-hosiptaList=[("1","BPKIHS"),("2","Vijaypur Hospital")]
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
+
+blood_group = [("1", "A+"), ("2", "A-"), ("3", "B+"), ("4", "B-"), ("5", "AB+"), ("6", "AB-"), ("3", "O+"), ("4", "O-")]
+genderList = [("1", "Male"), ("2", "Female"), ("3", "Others")]
+caseList = [("1", "Accident"), ("2", "Delivery"), ("3", "Anemia"), ("4", "Dialysis"), ("5", "Operation"),
+            ("6", "Urgent"), ("7", "Others")]
+hosiptaList = [("1", "BPKIHS"), ("2", "Vijaypur Hospital")]
+
 
 class Event(models.Model):
     title = models.CharField(max_length=20)
@@ -22,6 +24,7 @@ class Event(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+
 class Register(models.Model):
     register_id = models.OneToOneField(User, on_delete=models.DO_NOTHING, primary_key=True)
     name = models.CharField(max_length=50)
@@ -30,7 +33,7 @@ class Register(models.Model):
     number = models.PositiveIntegerField(unique=True)
     blood_group = models.CharField(choices=blood_group, max_length=2)
     image = models.ImageField(null=True, blank=True)
-    last_donated_date = models.DateField(null=True,blank=True)
+    last_donated_date = models.DateField(null=True, blank=True)
     is_verified = models.BooleanField(default=False)
     available_status = models.BooleanField(default=True)
     latitude = models.FloatField(default=0)
@@ -39,11 +42,14 @@ class Register(models.Model):
 
     def __str__(self):
         return f'{self.name}'
-        
+
+
 class BloodValidator(models.Model):
     request_id = models.CharField(max_length=5)
+
     def __str__(self):
         return f'{self.request_id}'
+
 
 class Blood(models.Model):
     register_id = models.ForeignKey(Register, null=True, blank=True, on_delete=models.DO_NOTHING)
@@ -56,22 +62,30 @@ class Blood(models.Model):
     hospital = models.CharField(choices=hosiptaList, max_length=20)
     required_date = models.DateField()
     status = models.BooleanField(default=False)
-    def save(self, *args, **kwargs):
-        if self.required_date < datetime.date.today():
-            raise ValidationError("The date cannot be in the past!")
-            return HttpResponse('Exception: DataNot Found')
-                
-        #else:
-            #except Exception as e:
-            #return HttpResponse('Exception: DataNot Found')
 
-        #return HttpResponse(save)
-                #self.message_user(request, 'Error changing model: %s' % e.msg, level=logging.ERROR)
-            
-        super(Event, self).save(*args, **kwargs)
-    
+    # def save(self, *args, **kwargs):
+    #     if self.required_date < datetime.date.today():
+    #         raise ValidationError("The date cannot be in the past!")
+    #         return HttpResponse('Exception: DataNot Found')
+    #
+    #     #else:
+    #         #except Exception as e:
+    #         #return HttpResponse('Exception: DataNot Found')
+    #
+    #     #return HttpResponse(save)
+    #             #self.message_user(request, 'Error changing model: %s' % e.msg, level=logging.ERROR)
+    #
+    #     super(Event, self).save(*args, **kwargs)
+
     def __str__(self):
         return f'{self.patient_name}'
+
+    def save(self, *args, **kwargs):
+        if self.required_date < datetime.date.today():
+            raise ValidationError('Required date cannot be previous date', code='invalid')
+
+        super(Blood, self).save(*args, **kwargs)
+
 
 class Donation(models.Model):
     donor_id = models.ForeignKey(Register, on_delete=models.DO_NOTHING)
@@ -84,4 +98,4 @@ class Donation(models.Model):
         unique_together = ['date', 'donor_id', 'request_id']
 
     def __str__(self):
-        return f'{self.request_id}, {self.donor_id}'   
+        return f'{self.request_id}, {self.donor_id}'
